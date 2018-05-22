41b8b990db1a20d2d512bdab014caf96f0433f0e
marie <marie-sarah@live.com>
2018-05-22 09:47:29 -0400

    commenting, added array to store data
    - added empty array to loop through and store data
    - added some commenting...

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,37 +1,62 @@
 $(document).ready(function() {
 
+id = []; // empty array... for our data!
+
   var resultData = function(data) {
+    //looping through the data returned on the first call to the API
     $.each(data.result, function(i, data) {
       if (data.image_url === null || data.tasting_note === null || data.tags.includes('lug tread')) {
       } else {
-        var storeData = function(data) {
-          $.each(data.result, function(i, data) {
-            var store = data.name;
-          })
-        }
+        // we don't want any beer that has no image, no description, or if the tags include 'lug tread'
         var image = data.image_url;
         var name = data.name;
-        var id = data.id;
         var tasting_note = data.tasting_note;
+        //let's append our data to the page
         $('#beauData').append("<div class='beau--child'><img class='beau--img' src=" + image + "><p>" + name + "</p><div><button class='beau--desc__button'>Click here to see more</button><div class='beau--desc hidden'><p>" + tasting_note + "</p><p>Stores with stock:</p><ul><li></li></ul></div></div></div>")
       }
     })
-
+// description button, displays data.tasting_note and the list of stores that have the current product
     $('button').click(function() {
+      //using this because we only want to open the current item
       $(this).next().toggleClass('hidden open');
       if ($(this).hasClass('open')) {
         console.log($(this).prev('button'))
       }
     })
   }
-
   jQuery.ajax({
+    //we only want the first 30 items - '&per_page=30'
     url: 'https://lcboapi.com/products?q=beaus/&per_page=30',
+    //async because we're completing calls synchronously
+    async: false,
     headers: {
+      // auth token header for API
       'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
     },
     success: function(data) {
-      resultData(data);
+      resultData(data)
+      //looping through data that's been returned so we can grab the values we need
+          $.each(data.result, function(i, data) {
+            if (data.image_url === null || data.tasting_note === null || data.tags.includes('lug tread')) {
+            } else {
+              id.push(data.id)
     }
-  })
+    })
+    }
+    //after this call is completed, let's do the next call to the api
+  }).then(function() {
+      $.each(data.result, function(i, data) {
+    jQuery.ajax({
+      // check what stores carry this product
+      url: 'lcboapi.com/stores?product_id=' + id,
+      async: false,
+      headers: {
+        'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
+      },
+      success: function(data) {
+          storeData(data);
+  }
+})
+})
+})
 });
