d75d9d267683c2883ab9361eb06c862bfd9570e4
marie <marie-sarah@live.com>
2018-09-05 17:46:47 -0400

    re-wrote most of this as it was an old project and I know better now.

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,23 +1,36 @@
 $(document).ready(function() {
-  products = [];
-  var resultData = function(data) {};
-  jQuery.ajax({
-    url: 'https://lcboapi.com/products?q=beaus/&per_page=30',
-    async: true,
-    headers: {
-      'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
-    },
-    success: function(data) {
-      resultData(data)
-      $.each(data.result, function(i, data) {
-        products.push({
-          title: data.name,
-          id: data.id,
-          image: data.image_url,
-          description: data.tasting_note
+     products = [];
+      var resultData = function(data) {};
+      jQuery.ajax({
+          url: 'https://lcboapi.com/products?q=beaus/&per_page=30',
+          async: true,
+          headers: {
+            'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
+          },
+          success: function(data) {
+            $.each(data.result, function(i, data) {
+              products.push({
+                name: data.name,
+                id: data.id,
+                image: data.image_url,
+                description: data.tasting_note,
+                stores: []
+              })
+            })
+          }
+        }).then(function() {
+            $.each(products, function(i, products) {
+                jQuery.ajax({
+                    url: 'https://lcboapi.com/stores?product_id=' + products.id,
+                    headers: {
+                      'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
+                    },
+                    success: function(data) {
+                      $.each(data.result, function(i, data) {
+                        products.stores.push({name:data.name})
+                      })
+                    }
+                })
+            })
+          })
         })
-      })
-    }
-  })
-          console.log(products);
-})
