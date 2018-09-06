e23d54db7d93e19933ea8d3638ef2e8cc76a77fe
marie <marie-sarah@live.com>
2018-09-05 21:02:20 -0400

    search works and function to place results in console works

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,36 +1,52 @@
 $(document).ready(function() {
-     products = [];
-      var resultData = function(data) {};
-      jQuery.ajax({
-          url: 'https://lcboapi.com/products?q=beaus/&per_page=30',
-          async: true,
+
+  $('#submit').click(function(e) {
+    e.preventDefault();
+    searchFunction();
+  })
+
+  function searchFunction() {
+    products = [];
+    var resultData = function(data) {};
+    jQuery.ajax({
+      url: 'https://lcboapi.com/products?q=' + $('input').val() + '/&per_page=30',
+      async: true,
+      headers: {
+        'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
+      },
+      success: function(data) {
+        $.each(data.result, function(i, data) {
+          products.push({
+            name: data.name,
+            id: data.id,
+            image: data.image_url,
+            description: data.tasting_note,
+            stores: []
+          })
+        })
+      }
+    }).then(function() {
+      $.each(products, function(i, products) {
+        jQuery.ajax({
+          url: 'https://lcboapi.com/stores?product_id=' + products.id,
           headers: {
             'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
           },
           success: function(data) {
             $.each(data.result, function(i, data) {
-              products.push({
-                name: data.name,
-                id: data.id,
-                image: data.image_url,
-                description: data.tasting_note,
-                stores: []
+              products.stores.push({
+                name: data.name
               })
             })
           }
-        }).then(function() {
-            $.each(products, function(i, products) {
-                jQuery.ajax({
-                    url: 'https://lcboapi.com/stores?product_id=' + products.id,
-                    headers: {
-                      'Authorization': 'Token MDo0YmEyZTA4Mi00YmUyLTExZTgtYjE5MC1jZmRmNTI4ZTVjNTQ6NVJEWnR4Y0FwV3pYdERMTVdCZTNxcWdBVnVvU1czWTEyS1FQ'
-                    },
-                    success: function(data) {
-                      $.each(data.result, function(i, data) {
-                        products.stores.push({name:data.name})
-                      })
-                    }
-                })
-            })
-          })
         })
+      })
+      var displayResults = (function() {
+        console.log(products);
+        $.each(products, function(i, products) {
+          console.log(products.name);
+        })
+      })();
+    })
+  }
+})
